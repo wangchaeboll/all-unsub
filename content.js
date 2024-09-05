12e844d211c1b71336fea8bc0fba733d06bbb803
wangchaeboll <wang.chaeboll@gmail.com>
2024-09-05 17:08:37 +0800

    dalam process memahami hahah

--- content.js
+++ content.js
@@ -13,10 +13,21 @@ window.addEventListener("load", () => {
     const dropBox = () => {
         const dropBoxx = document.querySelector("body > ytd-app > ytd-popup-container > tp-yt-iron-dropdown")
         return new Promise( (res,rej) => {
-            if (dropBoxx.hasAttribute("aria-hidden") && dropBox){
-                res()
+            if (dropBoxx && !dropBoxx.hasAttribute("aria-hidden") ){
+                res("first wave succeed")
             }else{
-                rej()
+                rej("first wave failed")
+            }
+        })
+    }
+
+    const konfurm = () => {
+        const muncul = document.getElementById("main")
+        return new Promise((res, rej) => {
+            if (muncul && !muncul.hasAttribute("aria-hidden") ){
+                res("second wave succeed")
+            }else{
+                rej("second wave failed")
             }
         })
     }
@@ -25,15 +36,20 @@ window.addEventListener("load", () => {
     const start = async () => {
 
         for (const elem of alam){
+            // unsub tray
             elem.querySelector("#notification-preference-button > ytd-subscription-notification-toggle-button-renderer-next > yt-button-shape > button > yt-touch-feedback-shape > div > div.yt-spec-touch-feedback-shape__fill").click();
-            await dropBox()
+            const resultsatu = await dropBox()
+            // 4th child
             document.querySelector("#items > ytd-menu-service-item-renderer:nth-child(4) > tp-yt-paper-item").click()
-            await
-            document.querySelector("#confirm-button > yt-button-shape > button > yt-touch-feedback-shape > div").click()
+            // const resultdua = await konfurm()
+            // confirm 
+            // document.querySelector("#confirm-button > yt-button-shape > button > yt-touch-feedback-shape > div").click()
+
+
         }
     }
 
-    if(a){
+    if(alam){
         setTimeout(() => {
             start()
         }, 400)
@@ -57,3 +73,48 @@ button.addEventListener("click", () => {
 const a = document.querySelector("body > ytd-app > ytd-popup-container > tp-yt-iron-dropdown")
 
 console.log(a)
+
+
+
+
+const elem = document.querySelectorAll("#grid-container > ytd-channel-renderer") 
+
+
+const dropBox = () => { 
+        console.log("hello")
+        return new Promise( (res,rej) => {
+            const dropBoxx = document.querySelector("body > ytd-app > ytd-popup-container > tp-yt-iron-dropdown")
+            if (dropBoxx && !dropBoxx.hasAttribute("aria-hidden") ){
+                console.log("yeayy")
+                res("first wave succeed")
+            }else{
+                console.log("nonnononno")
+                rej("first wave failed")
+            }
+        })
+}
+
+const getDropAttribute = () => {
+    const dropBoxx = document.querySelector("body > ytd-app > ytd-popup-container > tp-yt-iron-dropdown")
+    if (dropBoxx && !dropBoxx.hasAttribute("aria-hidden") ){
+        console.log("yeayy")
+        return true
+    }else{
+        console.log("nonnononno")
+       return false
+    }
+}
+
+function next() {
+    elem[1].querySelector("#notification-preference-button > ytd-subscription-notification-toggle-button-renderer-next > yt-button-shape > button > yt-touch-feedback-shape > div > div.yt-spec-touch-feedback-shape__fill").click();
+    setTimeout( () => getDropAttribute(), 500)
+    await 
+    if(getDropAttribute === true){
+        console.log("tunggu")
+    }else{
+        console.log("tak tunggu")
+    }
+
+}
+
+next()
